0e842bbb4dde466fd9fee52fe6af8a595d997f3a
Nibras Sameeh <nibras.s@aeturnum.com>
2026-05-12 17:35:09 +0530

    Add CommandLineRunner to initialize ROLE_USER if not present

--- src/main/java/com/nibras/app/Application.java
+++ src/main/java/com/nibras/app/Application.java
@@ -1,13 +1,32 @@
 package com.nibras.app;
 
+import com.nibras.app.role.Role;
+import com.nibras.app.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final RoleRepository roleRepository) {
+        return args -> {
+            final Optional<Role> role = roleRepository.findByName("ROLE_USER");
+            if (role.isEmpty()) {
+                final Role newRole = new Role();
+                newRole.setName("ROLE_USER");
+                newRole.setCreatedBy("system");
+                roleRepository.save(newRole);
+            }
+        };
     }
 
 }
